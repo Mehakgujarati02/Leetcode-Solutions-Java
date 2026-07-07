940f4c9fe52bc897e7da6220b82ff0267f297c42
Mehak <mehakthakur2209@gmail.com>
2026-07-07 22:42:16 +0530

    Time: 0 ms (100%), Space: 41.9 MB (69.7%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    int[] dp= new int[31];//as on the ques its given <=30 
+    /*int[] dp= new int[31];//as on the ques its given <=30 
     public int fib(int n) {
 
         if(n<= 1){
@@ -11,5 +11,22 @@ class Solution {
         dp[n]= fib(n-1) + fib(n-2);
         
     return dp[n];
-    } 
+    } */
+
+    //space optimizing it
+    public int fib(int n){
+        if(n <= 1){
+            return n;
+        }
+
+        int prev2= 0;//F(0)
+        int prev1= 1;//F(1)
+
+        for(int i=2; i<= n; i++){
+            int curr= prev1 + prev2;
+            prev2= prev1;
+            prev1= curr;
+        }
+        return prev1;
+    }
 }
