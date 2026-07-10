32fbce215e38234681bdeb77730b3b05a04a0319
Mehak <mehakthakur2209@gmail.com>
2026-07-10 21:02:24 +0530

    Time: 4 ms (5.97%), Space: 49.3 MB (5.49%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        LinkedList<String> res= new LinkedList<String>();//this is made to manage all the geenerated combinations
+        if(digits == null || digits.length()== 0){
+            return res;
+        }
+
+        //using bfs
+        String[] mapping = new String[] {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        res.add("");
+        while(res.peek().length() != digits.length()){
+            String remove = res.remove();
+            String map = mapping[digits.charAt(remove.length())- '0'];
+            for(char c: map.toCharArray()){
+                res.addLast(remove + c);
+            }
+        }
+
+        return res;   
+    }
+}
