966bff71b67d1470bdc65a0ecf496cf6c74207f0
Mehak <mehakthakur2209@gmail.com>
2026-07-18 18:23:16 +0530

    Time: 0 ms (100%), Space: 45.2 MB (95.76%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,37 @@
+class Solution {
+    List<List<Integer>> res= new ArrayList<>();
+
+    public void getPerm(int[] nums, int index){
+        //here is the base case
+        if(index== nums.length){
+            List<Integer> ans= new ArrayList<>();//here instead of passing the list in the rscursion call everytime that this condition hits we make a list and store the elements in the ans list, this helps keep the auxilary space as O(n)
+
+            for(int num: nums){
+                ans.add(num);
+            }
+            res.add(ans);
+            return;//as we dont want to recurse further
+        }
+
+        for(int i= index; i< nums.length; i++){
+
+            swap(nums, index, i);//swap index with the i, here doing swap instead of making new array to space optimize
+
+            getPerm(nums, index+ 1);//recurse to the other index
+
+            swap(nums, index, i);//this is the backtracking step where we swap the already swaped elements for further calculation
+        }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp= nums[i];
+        nums[i]= nums[j];
+        nums[j]= temp;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        getPerm(nums, 0 );
+        return res;
+    }
+}
+//tc:- O(n *n!) and sc:- O(n)
