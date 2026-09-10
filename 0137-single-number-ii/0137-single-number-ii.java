0c0e941497da3e05a378f1d5a048fe315ee366bf
Mehak <mehakthakur2209@gmail.com>
2026-09-11 02:41:45 +0530

    Time: 1 ms (93.12%), Space: 45.3 MB (62.83%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> freq= new HashMap<>();
+      /*  HashMap<Integer, Integer> freq= new HashMap<>();
         for(int i= 0; i< nums.length; i++){
             freq.put(nums[i], freq.getOrDefault(nums[i], 0)+ 1);
         }//here we got the freq map, so we now know which element appears how much time
@@ -14,4 +14,19 @@ class Solution {
         }
         return 0;
     }
-}//tc :- O(n) and sc:- O(n)
+}tc :- O(n) and sc:- O(n) 
+*/
+
+//trying to solve it using bit manipulation to optimize it further
+    //ones stores bits that have appeared exactly once so far, and twos stores bits that have appeared exactly twice so far.
+        int ones= 0;
+        int twos= 0;
+
+        for(int i=0;i<nums.length;i++){
+            ones=((nums[i]^ones) & (~(twos)));
+            twos=((nums[i]^twos) & (~(ones)));
+        }
+        return ones;
+
+    }    
+}// tc:- O(n) and sc:- O(1)
